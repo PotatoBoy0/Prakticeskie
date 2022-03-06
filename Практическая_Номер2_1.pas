ae048890a6ba78ee97d35a23405aa7954b0f6f49
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-03-07 01:36:17 +0300

    Add files via upload

---
+++ Практическая_Номер2_1.pas
@@ -0,0 +1,9 @@
+﻿program Практическая_Номер2_1;
+var
+ y:Real;
+ x:=ReadReal('Введите X');
+begin
+  if x>=2 then y:=(2*sqr(x)-1)
+  else y:=(x-3);
+  print (y);
+end.
---
+++ Практическая_Номер2_2.pas
@@ -0,0 +1,10 @@
+﻿program Практическая_Номер2_2;
+var
+ y:Integer;
+ x:=ReadInteger('Введите X');
+begin
+  if x<-4 then y:=(x+5);
+  if (-4<=x) and (x<=5)  then y:=(7*x-1);
+  if x>5 then y:=(1-sqr(x));
+  print (y);
+end.
