a4b1a56306f18fc9e7004dd79f60123cdbd8bf6a
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-03-07 11:52:38 +0300

    Add files via upload

---
+++ Практическая_4_1.pas
@@ -0,0 +1,13 @@
+﻿program Практическая_4_1;
+var
+  y,i:real;
+  (b,n) := ReadInteger2 ('Введите два числа');
+  begin
+    i:=1;
+    y:= ((4*b*i)/(1+i*cos(i)));
+    print (round(y));
+    begin
+      for var i:=y to n do
+      y:=y+y
+    end;
+  end.
