3d2159bff4185daba6ddb9cdefb4650eaca60552
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-03-21 16:28:40 +0300

    Add files via upload

---
+++ Практическая_4_3.pas
@@ -0,0 +1,11 @@
+﻿program  Практическая_4_3;
+var b:=ReadInteger ('Введите кол-во элементов');
+var k:=ReadInteger ('Введите шаг прогрессии');
+var t:integer;
+begin
+  for var i:=1 to b do
+  begin
+    t+=k;
+  end;
+  print (t);
+end.
