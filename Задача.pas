b05f4e91763de4f2ef31241baf80b33399dcf5c5
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-03-23 19:24:17 +0300

    Add files via upload

---
+++ Задача.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var a_max:integer;
+  for var i:=1 to 5 do
+    begin
+      var a:= random(-99,99);
+      println ('Рандомное число=',a,'После сложения=', abs((a div 10) + (a mod 10)));
+      if a>a_max then a_max:=a;
+    end;
+    print ('Макимальное число',a_max);
+end.
