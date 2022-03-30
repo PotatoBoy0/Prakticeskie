59dd4df39a314bd90cdfb6924c23cbf43ad5fd17
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-03-30 16:32:17 +0300

    Add files via upload

---
+++ Раздаточный№75(2).pas
@@ -0,0 +1,15 @@
+﻿{Рассчитать и вывести периметр и площадь прямоугольника.
+Расчеты оформить в процедуре.}
+procedure Raschet (a,b:integer;var S,P:integer);
+begin
+  S:= a*b;
+  P:= (a+b)*2;
+end;
+begin
+var  x,y,plos,perim:integer;
+  println ('Введите длину и ширину прямоугольника');
+  (x,y):=ReadInteger2;
+  Raschet (x,y,plos,perim);
+  println ('Площадь равна',plos);
+  print ('Периметр равен',perim);
+end.
