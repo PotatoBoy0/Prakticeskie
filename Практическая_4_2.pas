fa693bb11e3fb6a7899ed0e1942a9a910d81303e
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-04-02 14:36:46 +0300

    Add files via upload

---
+++ Практическая_4_2.pas
@@ -0,0 +1,12 @@
+﻿//Найти сумму двухзначных чисел, в которых есть цифры, меньше заданной.
+var zadannoe:=ReadInteger ('Введите число');
+var summa:integer;
+begin
+  for var i:=10 to 99 do
+  begin
+    if (i mod 10 <zadannoe) or (i div 10 <zadannoe)then
+      summa+=i else continue;
+      println ('Номер цикла',i);
+      println ('После суммы',summa);
+  end;
+end.
