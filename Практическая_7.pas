52f33c2d82ea2b73a20e667a28bb0ab35d108c0f
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-04-27 12:34:38 +0300

    Update and rename KapecHerna2.pas to Практическая_7.pas

---
+++ Практическая_7.pas
@@ -0,0 +1,48 @@
+{
+Задать массив, состоящий  из 10 вещественных чисел.
+Найти максимальный элемент массива.
+Увеличить в 0,5 раза каждый элемент массива с четным индексом.
+Найти произведение ненулевых элементов массива.
+}
+var i,max: integer;
+var k:integer;
+Массив:array[1..10] of integer;
+procedure maximum(i,max:integer);
+begin
+  for  i:=1 to 10 do
+  Массив[i]:=random (-100,100);
+    begin
+      max:=Массив[1];
+      for i:= 1 to 10 do 
+        begin
+          if Массив[i]>=max then max:=Массив[i];
+          println (Массив[i]);
+        end;
+      println('Максимальное число=',max);
+    end;
+end;
+procedure multiply (i:integer);
+  begin
+    for  i:=1 to 10 do
+      begin
+         Массив[i]:=random (-100,100);
+         if not odd(i) then
+         println ('Ответ', Массив[i]*0.5);
+      end;
+  end;
+procedure proizvedenie (i:integer);
+begin
+  for i:=1 to 10 do
+    begin
+      Массив[i]:=random (-100,100);
+      if i <> 0 then 
+      println ('Произведение массива =', Массив[i]*Массив[i]);
+    end;
+end;
+begin
+  maximum(i,max);
+  println ('-------------------------');
+  multiply (i);
+  println ('-------------------------');
+  proizvedenie (i);
+end.
