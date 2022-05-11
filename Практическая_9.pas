595c5969ba1e95adb832aae2a63ad14b57831ab1
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-05-11 15:36:30 +0300

    Update Практическая_9.pas

--- Практическая_9.pas
+++ Практическая_9.pas
@@ -1,28 +1,21 @@
 {
-Дана матрица А(4х4), состоящая из целых чисел.
-Ввести элементы матрицы, а затем вывести их.
-Определить сумму неотрицательных элементов матрицы.
-Заменить элементы исходной матрицы на числа, противоположные по знаку.
+Организовать массив А (3x3), состоящий из символов. Найти и вывести на экран:
+1. Исходный массив символов.
+2. Количество букв в массиве.
+3. Символы пунктуации заменить на цифру, соответствующую номеру строки, в
+которой находится заменяемый символ.
+4. Количество символов, принадлежащих к группе букв.
 }
-var  MAS: array [1..4, 1..4] of integer;
-var i, j, minus, k, t: integer;
 begin
-  for i := 1 to 4 do
-    for j := 1 to 4 do
+var MAS:array [1..3,1..3] of char;
+var amount,punct:integer;
+  for var Wight:=1 to 3 do
+    for var Hight:=1 to 3 do
     begin
-      MAS[i, j] := random(10, -10);
-      println('Массив:', MAS[i, j]);
+      MAS[Hight,Wight]:= ReadlnChar('Введите символ');
+      amount:= MAS[Hight,Wight].IsLetter ? amount+1:amount;
+      punct:= char.IsPunctuation(MAS[Hight,Wight]) ? Hight:punct;
     end;
-  for i := 1 to 4 do
-    for j := 1 to 4 do
-    begin
-      t := MAS[i, j] * (-1);
-      println('Обратная матрица:', t);
-    end;
-  for i := 1 to 4 do
-    for j := 1 to 4 do
-    begin
-      if MAS[i, j] > 0 then k += MAS[i, j]
-    end;
-  print('Сумма неотрицательных эл-тов первойматрицы:', k)
+    println ('Кол-во букв',amount);
+    println ('Пунктуация',punct);
 end.
