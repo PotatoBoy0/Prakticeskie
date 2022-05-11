44f8e50b803367ae30107a16c6a24e9f0db4215a
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-05-11 15:30:48 +0300

    Update Практическая_9.pas

--- Практическая_9.pas
+++ Практическая_9.pas
@@ -4,25 +4,25 @@
 Определить сумму неотрицательных элементов матрицы.
 Заменить элементы исходной матрицы на числа, противоположные по знаку.
 }
-var MAS: array [1..4, 1..4] of integer;
-var i, j, minus,k,t: integer;
+var  MAS: array [1..4, 1..4] of integer;
+var i, j, minus, k, t: integer;
 begin
   for i := 1 to 4 do
     for j := 1 to 4 do
     begin
       MAS[i, j] := random(10, -10);
-      println('Массив:',MAS[i, j]);
-    end;
-     for i := 1 to 4 do
-    for j := 1 to 4 do
-    begin
-      t:=MAS[i, j]*(-1);
-      println ('Обратная матрица:',t);
+      println('Массив:', MAS[i, j]);
     end;
   for i := 1 to 4 do
     for j := 1 to 4 do
     begin
-      if MAS[i, j] > 0 then k+=MAS[i, j]
+      t := MAS[i, j] * (-1);
+      println('Обратная матрица:', t);
     end;
-    print ('Сумма неотрицательных эл-тов первойматрицы:',k)
+  for i := 1 to 4 do
+    for j := 1 to 4 do
+    begin
+      if MAS[i, j] > 0 then k += MAS[i, j]
+    end;
+  print('Сумма неотрицательных эл-тов первойматрицы:', k)
 end.
