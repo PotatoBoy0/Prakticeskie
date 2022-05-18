10575bc9fdff95be63a83d879de05cc7e94bbba6
Kartofelny <68251110+PotatoBoy0@users.noreply.github.com>
2022-05-18 19:05:56 +0300

    Update Практическая_10.pas

--- Практическая_10.pas
+++ Практическая_10.pas
@@ -7,12 +7,17 @@
 нечетная);
 Удаляет букву «Y» в предложении.}
 begin
-  var (s1,s2,s3):=readlnstring3 ('Введите три слова');
-  var amount:string;
-  var s4:= Concat(s1 + '? ',s2 + '? ',s3 + '?').println;
-  println ('Колво символов после объединения:',Length(Concat(s1,s2,s3)));
-  for var i:=1 to Length(Concat(s1,s2,s3)) do
-  begin
-    
-  end;
+  var (s1, s2, s3) := readlnstring3('Введите три слова');
+  var s4 := Concat(s1 + '? ', s2 + '? ', s3 + '?').println;
+  println('Колво символов после объединения:', Length(s4));
+  var c := (round(Length(s4) / 2));
+  var b: string;
+  var del: integer;
+  var p: integer;
+  b := odd(c) ? (copy(s4, c, 1)) : (copy(s4, c, 2));
+  println('Символы посередине:', b);
+  p := pos('Y', s4);
+  del := pos('Y', s4);
+  if del > 0 then Delete(s4, p, 1);
+  print('Удаление ''Y'':', s4);
 end.
